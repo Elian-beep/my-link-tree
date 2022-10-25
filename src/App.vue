56bd630149c97b3e62e7dbcac78e52c159fa272c
Elian-beep <elianoliver101@gmail.com>
2022-10-25 10:40:15 -0300

    created project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <ContainerPage></ContainerPage>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ContainerPage from './components/ContainerPage.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: { ContainerPage }
+});
+</script>
+
+<style>
+#app {
+  font-family: 'Roboto', sans-serif;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/ContainerPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ContainerPage'
+});
+</script>
+
+<style scoped>
+.container{
+    height: 100vh;
+    max-width: 100vw;
+    background: green;
+}
+</style>
