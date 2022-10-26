fa0ba951eded5c1e85b54437c57498d1424f0378
Elian-beep <elianoliver101@gmail.com>
2022-10-25 22:42:59 -0300

    add scroll y in content pag

--- src/components/ContentPage.vue
+++ src/components/ContentPage.vue
@@ -7,30 +7,42 @@
   
 <script lang="ts">
 import { defineComponent } from "vue";
-import HeaderPage from './HeaderPage.vue';
+import HeaderPage from "./HeaderPage.vue";
 import BodyPage from "./BodyPage.vue";
 
 export default defineComponent({
   name: "ContentPage",
-  components: { HeaderPage, BodyPage }
+  components: { HeaderPage, BodyPage },
 });
 </script>
   
 <style scoped>
-.content{
-    height: 95vh;
-    width: 90vw;
-    border-radius: 4px;
+.content {
+  height: 95vh;
+  width: 90vw;
+  border-radius: 4px;
 
-    background: rgb(246, 246, 246);
-    box-shadow: -1px 5px 15px 5px rgba(0,0,0,0.55);
+  background: rgb(246, 246, 246);
+  box-shadow: -1px 5px 15px 5px rgba(0, 0, 0, 0.55);
+
+  overflow-y: scroll;
+}
+
+.content::-webkit-scrollbar {
+  width: 5px;
+  border-radius: 10px;
+  background-color: #1c1c1c;
+}
+
+.content::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background-color: #363636;
 }
 
 @media screen and (min-width: 500px) {
-    .content{
-        height: 95vh;
-        width: 400px;
-
-    }
+  .content {
+    height: 95vh;
+    width: 400px;
+  }
 }
 </style>
