3541249ec54926893e56d240d06724645bf9ea09
Elian-beep <elian.19batista@gmail.com>
2024-09-25 20:24:19 -0400

    feat: atualização no link do linkedin

--- src/components/BodyPage.vue
+++ src/components/BodyPage.vue
@@ -2,7 +2,7 @@
   <div>
     <ButtonLink :isDark="isDark" textButton="Portifólio" icone="fa-regular fa-folder" inputLink="https://elianbatista.vercel.app"/>
     <ButtonLink :isDark="isDark" textButton="Github" icone="fa-brands fa-github" inputLink="https://github.com/Elian-beep" />
-    <ButtonLink :isDark="isDark" textButton="Linkedin" icone="fa-brands fa-linkedin" inputLink="https://www.linkedin.com/in/elian-batista-beep/" />
+    <ButtonLink :isDark="isDark" textButton="Linkedin" icone="fa-brands fa-linkedin" inputLink="https://www.linkedin.com/in/elian-batista/" />
     <ButtonLink :isDark="isDark" textButton="E-mail" icone="fa-regular fa-envelope" inputLink="mailto:elian.19batista@gmail.com" />
     <ButtonLink :isDark="isDark" textButton="Instagram" icone="fa-brands fa-instagram" inputLink="https://www.instagram.com/e_lianb/" />
   </div>
